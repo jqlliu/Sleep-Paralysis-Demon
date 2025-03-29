1e49386c424f0312d61f2c1ce2e508b474162b91
jqlliu <2260216963liu@gmail.com>
2025-03-29 16:06:12 -0400

    Changed

--- main.py
+++ main.py
@@ -17,7 +17,9 @@ import socket
 import threading
 
 import math
-from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+
 
 host = ""
 port = 8000
@@ -564,6 +566,10 @@ if not testing:
     #server.serve_forever()
     pass
 
+
+
+
+
 #timer_thread = threading.Thread(target=wrap_timer)
 #timer_thread.start()
 #asyncio.run(timer())
@@ -574,3 +580,22 @@ def main() -> None:
 if __name__ == '__main__':
    main()
 
+print("SERVER UP")
+from http.server import HTTPServer, BaseHTTPRequestHandler
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        try:
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+        except:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(b'404 - Not Found')
+
+
+
+httpd = HTTPServer(('', 8000), SimpleHTTPRequestHandler)
+httpd.serve_forever()
