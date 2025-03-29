8e4f0583a77283af8ac21abd38b04a4967786ed2
jqlliu <2260216963liu@gmail.com>
2025-03-29 16:09:32 -0400

    Whoops wrong port

--- main.py
+++ main.py
@@ -597,5 +597,5 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
 
 
-httpd = HTTPServer(('', 8000), SimpleHTTPRequestHandler)
+httpd = HTTPServer(('', 8080), SimpleHTTPRequestHandler)
 httpd.serve_forever()
